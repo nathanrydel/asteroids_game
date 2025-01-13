26e5939533985eaba6e9a933a7646ce00c1fbb38
Nathan Rydel <nathan@nathanrydel.dev>
2025-01-12 20:40:03 -0700

    CH 2, L 3

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -13,6 +16,7 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
